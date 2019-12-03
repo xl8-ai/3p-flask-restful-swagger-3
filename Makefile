be270fe6f34dab59bb8b8a61e0eca19b5d53b86b
jonathan <jonathan.ullindah@gmail.com>
2019-12-03 23:45:01 +0100

    Update version

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 release:
-	git tag -a `python setup.py --version` -m "Releasing to https://pypi.python.org/pypi/flask-restful-swagger-2/"
+	git tag -a `python setup.py --version` -m "Releasing to https://pypi.python.org/pypi/flask-restful-swagger-3/"
 	git push --tags
 	rm -rf dist
 	python setup.py sdist
--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
-with open('README') as file:
-    long_description = file.read()
 
-setup(name='flask-restful-swagger-2',
-      version='0.35',
-      url='https://github.com/swege/flask-restful-swagger-2.0',
+def read(file_to_read):
+    with open(file_to_read, 'r') as f:
+        return f.read()
+
+
+setup(name='flask-restful-swagger-3',
+      version='0.1',
+      url='https://gitlab.com/john-ull/framework/flask-restful-swagger-3',
       zip_safe=False,
-      packages=['flask_restful_swagger_2'],
+      packages=['flask_restful_swagger_3'],
       package_data={
-        'flask_restful_swagger_2': [
+        'flask_restful_swagger_3': [
         ]
       },
-      description='Extract swagger specs from your flask-restful project. Project based on flask-restful-swagger by Ran Tavory.',
-      author='Soeren Wegener',
+      description='Extract swagger specs from your flask-restful project.'
+                  ' Project based on flask-restful-swagger-2 by Soeren Wegener.',
+      author='Jonathan ULLINDAH',
       license='MIT',
-      long_description=long_description,
-      install_requires=['Flask-RESTful>=0.2.12'],
+      long_description=read('README.md'),
+      install_requires=['Flask-RESTful>=0.3.7'],
       tests_require=['nose'],
       test_suite='nose.collector'
       )
